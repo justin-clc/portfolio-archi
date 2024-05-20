f83b480909b520e7c70287aa1c270194b07899ae
Anzel Sakamoto <anzelkensakamoto@yahoo.com.ph>
2024-05-20 21:06:03 +0800

    FeaturedProject button color changes

--- src/pages/home sections/FeaturedProjects.js
+++ src/pages/home sections/FeaturedProjects.js
@@ -35,7 +35,7 @@ export default function FeaturedProjects() {
       </div>
       <section className="bg-bgDark px-10 py-10">
         <h3 className="font-primary m-auto max-w-5xl text-center text-3xl font-bold text-white md:text-4xl">
-          Recent Projects
+          Featured Projects
         </h3>
         <Carousel />
       </section>
@@ -43,14 +43,14 @@ export default function FeaturedProjects() {
       <section className="bg-bgDark px-10 py-8">
         <FeaturedGallery />
         <button
-          class="group relative m-auto mt-6 flex h-16  w-64 cursor-pointer items-center justify-center overflow-hidden rounded-md bg-accent p-2 font-extrabold text-white duration-300 hover:scale-95"
+          className="group relative m-auto mt-6 flex h-16  w-48 cursor-pointer items-center justify-center overflow-hidden rounded-md bg-accent p-2 font-extrabold text-white duration-300 hover:scale-95"
           onClick={goToSchoolYears}
         >
-          <div class="absolute -top-4 right-32  z-10 h-40 w-40 rounded-full bg-primary duration-500 group-hover:right-2 group-hover:top-1 group-hover:scale-150"></div>
-          <div class="absolute -top-4 right-2  z-10 h-32 w-32 rounded-full bg-sky-800 duration-500 group-hover:right-2  group-hover:top-1 group-hover:scale-150"></div>
-          <div class="absolute -right-12 top-4 z-10 h-24 w-24 rounded-full bg-sky-700 duration-500 group-hover:right-2  group-hover:top-1 group-hover:scale-150"></div>
-          <div class="absolute -top-4 right-20 z-10 h-16 w-16 rounded-full bg-sky-600 duration-500 group-hover:right-2  group-hover:top-1 group-hover:scale-150"></div>
-          <p class="z-10">See more</p>
+          <div className="absolute -top-4 right-16  z-10 h-32 w-32 rounded-full bg-primary duration-500 group-hover:right-2 group-hover:top-1 group-hover:scale-150"></div>
+          <div className="absolute -top-4 right-2  z-10 h-24 w-24 rounded-full bg-sky-800 duration-500 group-hover:right-2  group-hover:top-1 group-hover:scale-150"></div>
+          <div className="absolute -right-12 top-4 z-10 h-16 w-16 rounded-full bg-sky-700 duration-500 group-hover:right-2  group-hover:top-1 group-hover:scale-150"></div>
+          <div className="absolute -top-4 right-20 z-10 h-8 w-8 rounded-full bg-sky-600 duration-500 group-hover:right-2  group-hover:top-1 group-hover:scale-150"></div>
+          <p className="z-10">See more</p>
         </button>
       </section>
     </>
