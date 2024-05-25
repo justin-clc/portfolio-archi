e10604bc3b3ea908061f8a0d01280ee64030a049
Anzel Sakamoto <anzelkensakamoto@yahoo.com.ph>
2024-05-25 23:22:30 +0800

    added semester filters for the school year page

--- src/pages/Subjects.js
+++ src/pages/Subjects.js
@@ -2,6 +2,7 @@ import { useState, useEffect } from "react";
 
 import Semester from "../components/Semester";
 import { useParams } from "react-router-dom";
+import SemesterDropdown from "../components/SemeterDropdown";
 
 export default function Subjects() {
   const { year } = useParams();
@@ -10,7 +11,7 @@ export default function Subjects() {
     "mb-3 me-3 rounded-full border border-primary bg-bgDark px-5 py-2.5 text-center text-base font-medium text-accent transition-colors hover:bg-primary hover:text-white focus:outline-none";
 
   const btnSelected =
-    "mb-3 me-3 rounded-full border border-primary bg-primary px-5 py-2.5 text-center text-base font-medium text-accent transition-colors hover:text-white focus:outline-none";
+    "mb-3 me-3 rounded-full border border-accent bg-primary px-5 py-2.5 text-center text-base font-medium text-accent transition-colors hover:text-white focus:outline-none";
 
   useEffect(() => {
     setTimeout(() => {
@@ -68,10 +69,16 @@ export default function Subjects() {
   console.log(semester1);
   console.log(semester2);
 
+  const [selectedSemester, setSelectedSemester] = useState("1");
+
+  const handleSemesterSelect = (selectedValue) => {
+    setSelectedSemester(selectedValue); // Update parent state
+  };
+
   return (
     <main className="-mt-16 w-full bg-bgMedium">
       <section className="bg-Medium bg-[url('https://flowbite.s3.amazonaws.com/docs/jumbotron/hero-pattern-dark.svg')] pt-32">
-        <div className="lg:pt-18 relative z-10 mx-auto max-w-screen-xl px-4 py-8 text-center lg:pb-24">
+        <div className="lg:pt-18 relative z-10 mx-auto max-w-screen-xl px-4 py-8 text-center lg:pb-12">
           {/* <a
             href="/portfolio-archi/#/test"
             className="mb-7 inline-flex items-center justify-between rounded-full bg-bgDark p-1.5 pr-3.5 text-sm text-bgLight transition-colors hover:bg-bgLight hover:text-primary"
@@ -98,63 +105,63 @@ export default function Subjects() {
               />
             </svg>
           </a> */}
-          <h1 className="font-primary mb-4 mt-12 text-4xl font-extrabold leading-none tracking-tight text-white md:text-5xl lg:text-6xl">
+          <h1 className="font-primary mt- mb-4 text-4xl font-extrabold leading-none tracking-tight text-white md:text-5xl lg:text-6xl">
             {genContent.SchoolYears?.heading + " " + year}
           </h1>
           <p className="font-secondary mb-8 text-lg font-normal text-bgLight sm:px-16 lg:px-48 lg:text-xl">
             {genContent.SchoolYears?.shortText}
           </p>
-        </div>
-
-        <div className="relative z-10 m-auto flex max-w-6xl flex-wrap items-center justify-center py-4 md:py-8">
-          <button
-            type="button"
-            className={year === "1" ? btnSelected : btnDefault}
-            onClick={() =>
-              (window.location.href = "/portfolio-archi/#/school-year/1")
-            }
-          >
-            1st Year
-          </button>
-          <button
-            type="button"
-            className={year === "2" ? btnSelected : btnDefault}
-            onClick={() =>
-              (window.location.href = "/portfolio-archi/#/school-year/2")
-            }
-          >
-            2nd Year
-          </button>
-          <button
-            type="button"
-            className={year === "3" ? btnSelected : btnDefault}
-            onClick={() =>
-              (window.location.href = "/portfolio-archi/#/school-year/3")
-            }
-          >
-            3rd Year
-          </button>
-          <button
-            type="button"
-            className={year === "4" ? btnSelected : btnDefault}
-            onClick={() =>
-              (window.location.href = "/portfolio-archi/#/school-year/4")
-            }
-          >
-            4th Year
-          </button>
+          <div className="relative z-10 m-auto flex max-w-6xl flex-wrap items-center justify-center py-4 md:py-8">
+            <button
+              type="button"
+              className={year === "1" ? btnSelected : btnDefault}
+              onClick={() =>
+                (window.location.href = "/portfolio-archi/#/school-year/1")
+              }
+            >
+              1st Year
+            </button>
+            <button
+              type="button"
+              className={year === "2" ? btnSelected : btnDefault}
+              onClick={() =>
+                (window.location.href = "/portfolio-archi/#/school-year/2")
+              }
+            >
+              2nd Year
+            </button>
+            <button
+              type="button"
+              className={year === "3" ? btnSelected : btnDefault}
+              onClick={() =>
+                (window.location.href = "/portfolio-archi/#/school-year/3")
+              }
+            >
+              3rd Year
+            </button>
+            <button
+              type="button"
+              className={year === "4" ? btnSelected : btnDefault}
+              onClick={() =>
+                (window.location.href = "/portfolio-archi/#/school-year/4")
+              }
+            >
+              4th Year
+            </button>
+          </div>
+          <SemesterDropdown onSelect={handleSemesterSelect} />
         </div>
 
         {semester1.length === 0 && semester2.length === 0 ? (
-          <h3 className="font-primary m-auto mt-20 max-w-5xl text-center text-3xl font-bold text-white md:text-4xl">
+          <h3 className="font-primary m-auto mt-8 max-w-5xl text-center text-3xl font-bold text-white md:text-4xl">
             No Subjects Yet
           </h3>
         ) : (
           <>
-            {semester1.length > 0 && (
+            {selectedSemester === "1" && semester1.length > 0 && (
               <Semester SemesterType={1} SemesterArray={semester1} />
             )}
-            {semester2.length > 0 && (
+            {selectedSemester === "2" && semester2.length > 0 && (
               <Semester SemesterType={2} SemesterArray={semester2} />
             )}
           </>
