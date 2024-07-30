72faac60c7f4c968a5c5c82e30a136938a09a1e1
Anzel Sakamoto <anzelkensakamoto@yahoo.com.ph>
2024-07-31 00:06:21 +0800

    Featured gallery size adjustment

--- src/components/FeaturedGallery.js
+++ src/components/FeaturedGallery.js
@@ -1,6 +1,6 @@
 export default function FeaturedGallery({ images }) {
   return (
-    <div className="m-auto max-w-6xl py-4">
+    <div className="m-auto max-w-7xl py-4">
       <div className="columns-1 gap-4 sm:columns-2 md:columns-3 lg:columns-4">
         {images.map((image) => (
           <div key={image.sys.id} className="mb-4 break-inside-avoid">
