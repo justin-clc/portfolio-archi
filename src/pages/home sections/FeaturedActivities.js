79a03b78fdefd53545174b9100985df1cb8e4cab
Anzel Sakamoto <anzelkensakamoto@yahoo.com.ph>
2024-10-20 23:06:55 +0800

    Landing section layout revision

--- src/pages/home sections/FeaturedActivities.js
+++ src/pages/home sections/FeaturedActivities.js
@@ -7,83 +7,64 @@ import FeaturedGallery from "../../components/FeaturedGallery";
 import { useEffect, useState } from "react";
 
 export default function FeaturedActivities() {
-  const navigate = useNavigate();
+	const navigate = useNavigate();
 
-  const goToSchoolYears = () => {
-    navigate("/school-year/1"); // Assuming this is a valid route
-  };
+	const goToSchoolYears = () => {
+		navigate("/school-year/1"); // Assuming this is a valid route
+	};
 
-  const waveDivider = {
-    width: "calc(100% + 1.3px)",
-  };
+	const [featured, setFeatured] = useState([]);
+	const [images, setImages] = useState([]);
+	useEffect(() => {
+		const contentful = require("contentful");
 
-  const [featured, setFeatured] = useState([]);
-  const [images, setImages] = useState([]);
-  useEffect(() => {
-    const contentful = require("contentful");
+		const client = contentful.createClient({
+			space: process.env.REACT_APP_CONTENTFUL_SPACE,
+			environment: process.env.REACT_APP_CONTENTFUL_ENVIRONMENT,
+			accessToken: process.env.REACT_APP_CONTENTFUL_TOKEN,
+		});
 
-    const client = contentful.createClient({
-      space: process.env.REACT_APP_CONTENTFUL_SPACE,
-      environment: process.env.REACT_APP_CONTENTFUL_ENVIRONMENT,
-      accessToken: process.env.REACT_APP_CONTENTFUL_TOKEN,
-    });
+		client
+			.getEntries({
+				content_type: "featuredActivities",
+			})
+			.then((entries) => {
+				setFeatured(entries.items);
+			})
+			.catch(console.error);
 
-    client
-      .getEntries({
-        content_type: "featuredActivities",
-      })
-      .then((entries) => {
-        setFeatured(entries.items);
-      })
-      .catch(console.error);
+		client
+			.getEntries({
+				content_type: "featuredImages",
+			})
+			.then((entries) => {
+				setImages(entries.items);
+			})
+			.catch(console.error);
+	}, []);
 
-    client
-      .getEntries({
-        content_type: "featuredImages",
-      })
-      .then((entries) => {
-        setImages(entries.items);
-      })
-      .catch(console.error);
-  }, []);
+	return (
+		<>
+			<section className="bg-bgDark px-10 py-10">
+				<h3 className="m-auto max-w-5xl text-center font-primary text-3xl font-bold text-white md:text-4xl">
+					Featured Projects
+				</h3>
+				<Carousel items={featured} />
+			</section>
 
-  return (
-    <>
-      <div className="relative w-full overflow-hidden bg-bgDark">
-        <svg
-          className="h-22 relative block"
-          style={waveDivider}
-          data-name="Wave Divider"
-          xmlns="http://www.w3.org/2000/svg"
-          viewBox="0 0 1200 120"
-          preserveAspectRatio="none"
-        >
-          <path
-            d="M321.39,56.44c58-10.79,114.16-30.13,172-41.86,82.39-16.72,168.19-17.73,250.45-.39C823.78,31,906.67,72,985.66,92.83c70.05,18.48,146.53,26.09,214.34,3V0H0V27.35A600.21,600.21,0,0,0,321.39,56.44Z"
-            className="fill-bgMedium"
-          ></path>
-        </svg>
-      </div>
-      <section className="bg-bgDark px-10 py-10">
-        <h3 className="font-primary m-auto max-w-5xl text-center text-3xl font-bold text-white md:text-4xl">
-          Featured Projects
-        </h3>
-        <Carousel items={featured} />
-      </section>
-
-      <section className="bg-bgDark px-10 py-8">
-        <FeaturedGallery images={images} />
-        <button
-          className="group relative m-auto mt-6 flex h-16  w-48 cursor-pointer items-center justify-center overflow-hidden rounded-md bg-accent p-2 font-extrabold text-white duration-300 hover:scale-95"
-          onClick={goToSchoolYears}
-        >
-          <div className="absolute -top-4 right-16  z-10 h-32 w-32 rounded-full bg-primary duration-500 group-hover:right-2 group-hover:top-1 group-hover:scale-150"></div>
-          <div className="absolute -top-4 right-2  z-10 h-24 w-24 rounded-full bg-sky-800 duration-500 group-hover:right-2  group-hover:top-1 group-hover:scale-150"></div>
-          <div className="absolute -right-12 top-4 z-10 h-16 w-16 rounded-full bg-sky-700 duration-500 group-hover:right-2  group-hover:top-1 group-hover:scale-150"></div>
-          <div className="absolute -top-4 right-20 z-10 h-8 w-8 rounded-full bg-sky-600 duration-500 group-hover:right-2  group-hover:top-1 group-hover:scale-150"></div>
-          <p className="z-10">See more</p>
-        </button>
-      </section>
-    </>
-  );
+			<section className="bg-bgDark px-10 py-8">
+				<FeaturedGallery images={images} />
+				<button
+					className="group relative m-auto mt-6 flex h-16  w-48 cursor-pointer items-center justify-center overflow-hidden rounded-md bg-accent p-2 font-extrabold text-white duration-300 hover:scale-95"
+					onClick={goToSchoolYears}
+				>
+					<div className="absolute -top-4 right-16  z-10 h-32 w-32 rounded-full bg-primary duration-500 group-hover:right-2 group-hover:top-1 group-hover:scale-150"></div>
+					<div className="absolute -top-4 right-2  z-10 h-24 w-24 rounded-full bg-sky-800 duration-500 group-hover:right-2  group-hover:top-1 group-hover:scale-150"></div>
+					<div className="absolute -right-12 top-4 z-10 h-16 w-16 rounded-full bg-sky-700 duration-500 group-hover:right-2  group-hover:top-1 group-hover:scale-150"></div>
+					<div className="absolute -top-4 right-20 z-10 h-8 w-8 rounded-full bg-sky-600 duration-500 group-hover:right-2  group-hover:top-1 group-hover:scale-150"></div>
+					<p className="z-10">See more</p>
+				</button>
+			</section>
+		</>
+	);
 }
--- src/pages/home sections/Landing.js
+++ src/pages/home sections/Landing.js
@@ -2,99 +2,86 @@ import { useNavigate } from "react-router-dom";
 import SkeletonLoader from "../../components/SkeletonLoader";
 
 export default function Landing({ content }) {
-  const navigate = useNavigate();
+	const navigate = useNavigate();
 
-  const goToSchoolYears = () => {
-    navigate("/school-year/1"); // Assuming this is a valid route
-  };
+	const goToSchoolYears = () => {
+		navigate("/school-year/1");
+	};
 
-  const landingHeight = {
-    height: "clamp(600px, 100dvh, 1024px)",
-  };
+	const landingHeight = {
+		height: "clamp(600px, 100dvh, 720px)",
+	};
 
-  const landingSvgStyle = {
-    width: "clamp(275px, 95dvw, 900px)",
-    maxHeight: "500px"
-  };
+	return (
+		<section
+			className="-mt-16 bg-bgDark px-8 py-24 md:pt-36"
+			style={landingHeight}
+		>
+			<div className="mx-auto flex h-full max-h-[480px] max-w-7xl flex-col-reverse items-center gap-8 md:max-h-[400px] md:flex-row">
+				{/* Landing Content */}
+				<div className="flex flex-col items-center justify-center md:ml-8 md:w-1/2 md:items-end">
+					{/* Work in progress */}
+					{/* <span className="text-center font-secondary text-lg text-accent md:text-right">
+						Architecture Student
+					</span> */}
 
-  return (
-    <section className="relative bg-secondary">
-      <img
-        src="https://images.unsplash.com/photo-1493397212122-2b85dda8106b?q=80&w=2071&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
-        alt=""
-        className="z-1 z-1 absolute h-full w-full bg-secondary object-cover opacity-25"
-      />
-      <div
-        className="l:gap-6 z-5 relative mx-auto box-border flex max-w-screen-2xl flex-col-reverse items-center px-10 pb-10 pt-20 md:flex-row md:justify-around md:gap-5 md:pb-0 md:pt-12"
-        style={landingHeight}
-      >
-        <div className="flex flex-col items-center md:ml-8 md:items-start">
-          <h1 className="font-primary l:text-6xl text-center text-4xl font-bold text-bgDark md:text-left md:text-5xl 2xl:text-7xl">
-            {!content.Me ? (
-              <SkeletonLoader type={"headingLg"} color={"dark"} />
-            ) : (
-              content.Me?.shortText
-            )}
-            {/* Use optional chaining to avoid the error */}
-          </h1>
-          {!content.ShortDesc ? (
-            <>
-              <SkeletonLoader type={"paragraph"} color={"medium"} />
-            </>
-          ) : (
-            <p className="font-secondary mt-2 text-center text-xl font-bold text-bgMedium md:text-left md:text-2xl">
-              {content.ShortDesc?.shortText}
-            </p>
-          )}
-          <button
-            className="font-primary group relative z-10 mt-6 h-12 w-48 cursor-pointer overflow-hidden rounded-md border-none bg-primary p-2 text-xl font-bold capitalize text-white transition-transform  hover:scale-105"
-            onClick={goToSchoolYears}
-          >
-            View my projects
-            <span className="absolute -left-2 -top-8 h-32 w-52 origin-bottom scale-x-0 transform rounded-full bg-blue-200 transition-transform duration-1000 group-hover:scale-x-100 group-hover:duration-500"></span>
-            <span className="absolute -left-2 -top-8 h-32 w-52 origin-bottom scale-x-0 transform rounded-full bg-blue-400 transition-transform duration-700 group-hover:scale-x-100 group-hover:duration-700"></span>
-            <span className="absolute -left-2 -top-8 h-32 w-52 origin-bottom scale-x-0 transform rounded-full bg-primary transition-transform duration-500 group-hover:scale-x-100 group-hover:duration-1000"></span>
-            <span className="absolute left-6 top-2.5 z-10 flex items-center opacity-0 duration-100 group-hover:opacity-100 group-hover:duration-1000">
-              <p className="-mt-px flex w-36 items-center justify-center gap-3">
-                Let's Go
-                <svg
-                  fill="currentColor"
-                  className="inline h-8 w-8"
-                  viewBox="0 0 24 24"
-                  xmlns="http://www.w3.org/2000/svg"
-                >
-                  <path d="M12 11V8l4 4-4 4v-3H8v-2h4Zm0-9c5.52 0 10 4.48 10 10s-4.48 10-10 10S2 17.52 2 12 6.48 2 12 2Zm0 18c4.42 0 8-3.58 8-8s-3.58-8-8-8-8 3.58-8 8 3.58 8 8 8Z" />
-                </svg>
-              </p>
-            </span>
-          </button>
-        </div>
+					{!content.Me ? (
+						<SkeletonLoader type={"headingLg"} />
+					) : (
+						<h1 className="text-center font-primary text-4xl font-bold text-white md:text-right md:text-5xl">
+							{content.Me?.shortText}
+						</h1>
+					)}
+					{!content.ShortDesc ? (
+						<>
+							<SkeletonLoader
+								type={"paragraph"}
+								color={"medium"}
+							/>
+						</>
+					) : (
+						<p className="text-l max-w-3/4 mt-2 text-center font-secondary font-bold text-bgLight md:text-right md:text-lg">
+							{content.ShortDesc?.shortText}
+						</p>
+					)}
 
-        <svg
-          id="landing-svg"
-          className="relative md:-mr-6"
-          style={landingSvgStyle}
-          viewBox="0 0 100 100"
-          xmlns="http://www.w3.org/2000/svg"
-        >
-          <path
-            id="landing-blob"
-            className="animate-pulse text-bgMedium"
-            fill="currentColor"
-            d="M23.3,-28.7C30.4,-21.7,36.8,-14.6,38.5,-6.5C40.2,1.6,37.4,10.8,32.4,18C27.4,25.3,20.2,30.6,11.8,34.5C3.5,38.4,-6.2,40.8,-13.9,38.1C-21.6,35.3,-27.5,27.3,-32,18.8C-36.6,10.3,-39.9,1.3,-38.7,-7C-37.5,-15.4,-31.7,-23.2,-24.5,-30.2C-17.3,-37.2,-8.7,-43.4,-0.3,-43C8,-42.6,16.1,-35.7,23.3,-28.7Z"
-            transform="translate(50 50)"
-            strokeWidth={0}
-          ></path>
-          <clipPath id="cp">
-            <use href="#landing-blob" />
-          </clipPath>
-          <image
-            className="relative h-auto w-full object-cover"
-            clipPath="url(#cp)"
-            href={content.ShortDesc?.image.fields.file.url}
-          />
-        </svg>
-      </div>
-    </section>
-  );
+					<button
+						type="button"
+						id="view-projects"
+						className="mt-4 flex h-fit w-44 cursor-pointer items-center justify-start gap-3 rounded-sm border-4 border-primary bg-primary"
+						onClick={goToSchoolYears}
+					>
+						<span className="text flex h-full w-4/5 items-center justify-center bg-bgDark py-1 font-bold text-white">
+							View Projects
+						</span>
+						<svg
+							className="arrow fill-white"
+							viewBox="0 0 448 512"
+							height="1em"
+							xmlns="http://www.w3.org/2000/svg"
+						>
+							<path d="M438.6 278.6c12.5-12.5 12.5-32.8 0-45.3l-160-160c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3L338.8 224 32 224c-17.7 0-32 14.3-32 32s14.3 32 32 32l306.7 0L233.4 393.4c-12.5 12.5-12.5 32.8 0 45.3s32.8 12.5 45.3 0l160-160z"></path>
+						</svg>
+					</button>
+				</div>
+
+				{/* Divider */}
+				<div className="relative hidden h-full min-h-96 w-1 items-center justify-between rounded-lg bg-white md:flex md:flex-col">
+					<div className=" block h-1 min-w-4 rounded-lg bg-white"></div>
+					<div className=" block h-1 min-w-4 rounded-lg bg-white"></div>
+				</div>
+
+				{/* Photo */}
+				{!content.ShortDesc ? (
+					<div className="min-h-52 min-w-52 animate-pulse rounded-2xl bg-bgLight md:min-h-80 md:min-w-80"></div>
+				) : (
+					<img
+						src={content.ShortDesc?.image.fields.file.url}
+						alt="Me"
+						className="max-w-52 rounded-lg md:h-80 md:max-w-none"
+					/>
+				)}
+			</div>
+		</section>
+	);
 }
